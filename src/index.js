a217470f7668da90787164029dd6de4ceca1dd0a
muyulong <mylyao@qq.com>
2024-07-18 13:33:04 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.muyulong.top": dockerHub,
+  "quay.muyulong.top": "https://quay.io",
+  "gcr.muyulong.top": "https://gcr.io",
+  "k8s-gcr.muyulong.top": "https://k8s.gcr.io",
+  "k8s.muyulong.top": "https://registry.k8s.io",
+  "ghcr.muyulong.top": "https://ghcr.io",
+  "cloudsmith.muyulong.top": "https://docker.cloudsmith.io",
+  "ecr.muyulong.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.muyulong.top": dockerHub,
 };
 
 function routeByHosts(host) {
